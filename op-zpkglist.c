a54c8ac71329fabddd1722a5837625e4bf6ca2b5
Alexey Tourbin <alexey.tourbin@gmail.com>
2018-04-13 05:45:02 +0300

    op-zpkglist.c: updated for jumbo frames
    There is still no OpView() method as such, as it would probably still
    need malloc() at least in the case when <il,dl> is not aligned to a
    4-byte boundary and the architecture does not support unaligned access.
    But at least things work again.

--- op-zpkglist.c
+++ op-zpkglist.c
@@ -26,7 +26,11 @@ union readState {
 
 static ssize_t OP(Bulk)(struct zpkglistReader *z, void **bufp, const char *err[2])
 {
-    return zreader_getFrame(z->opState, bufp, NULL, err);
+    ssize_t ret = zreader_getFrame(z->opState, bufp, NULL, false, err);
+    // Materialize the imlicit magic bytes.
+    if (ret > 0)
+	ret += 8, *bufp -= 8;
+    return ret;
 }
 
 static ssize_t OP(Read)(struct zpkglistReader *z, void *buf, size_t size, const char *err[2])
@@ -85,8 +89,7 @@ static int64_t OP(ContentSize)(struct zpkglistReader *z)
     return zreader_contentSize(z->opState);
 }
 
-static ssize_t OP(NextView)(struct zpkglistReader *z,
-	void **bufp, int64_t *posp, const char *err[2])
+static ssize_t OP(NextMalloc)(struct zpkglistReader *z, int64_t *posp, const char *err[2])
 {
     if (!z->readState) {
 	z->readState = malloc(sizeof(union readState));
@@ -94,53 +97,60 @@ static ssize_t OP(NextView)(struct zpkglistReader *z,
 	    return ERRNO("malloc"), -1;
 	memset(z->readState, 0, sizeof(union readState));
     }
+    bool jumbo = false;
     struct headerReadState *s = &((union readState *) z->readState)->h;
     if (s->cur == s->end) {
-	ssize_t ret = zreader_getFrame(z->opState, (void **) &s->cur, &s->pos, err);
-	if (ret <= 0)
+	ssize_t ret = zreader_getFrame(z->opState, (void **) &s->cur, &s->pos, true, err);
+	if (ret == 0 || ret == -1)
 	    return ret;
+	// Jumbo frame?
+	if (ret < 0)
+	    ret = -ret, jumbo = true;
+	if (ret < 8)
+	    return ERRSTR("bad header size"), -1;
 	s->end = s->cur + ret;
 	s->ix = 0;
-	if (s->end - s->cur < 16)
-	    return ERRSTR("bad header size"), -1;
-	if (!headerCheckMagic(s->cur))
-	    return ERRSTR("bad header magic"), -1;
     }
-    ssize_t dataSize = headerDataSize(s->cur);
-    if (dataSize < 0 || 16 + dataSize > s->end - s->cur)
+    // The magic has been checked, s->cur points at <il,dl>.
+    // There is at least 8 bytes, to the probe for <il,dl> is valid.
+    ssize_t dataSize = headerDataSize(s->cur - 8);
+    if (dataSize < 0 || 8 + dataSize > s->end - s->cur)
 	return ERRSTR("bad data size"), -1;
     void *blob = s->cur;
-    s->cur += 16 + dataSize;
+    s->cur += 8 + dataSize;
+    // Is s->cur pointing to the end of the frame?
     if (s->cur != s->end) {
+	// Nope, it must be the next magic.
+	if (jumbo) {
+	    // Jumbo frames must contain only one header.
+	    free(blob), s->cur = s->end = NULL;
+	    return ERRSTR("bad jumbo size"), -1;
+	}
 	if (s->end - s->cur < 16)
 	    return ERRSTR("bad header size"), -1;
 	if (!headerCheckMagic(s->cur))
 	    return ERRSTR("bad header magic"), -1;
+	s->cur += 8;
     }
+    // Combine file offset + header no in a frame => logical offset.
     if (s->ix > 3)
 	return ERRSTR("too many headers in a frame"), -1;
     if (posp)
 	*posp = ((int64_t) s->pos << 2) + s->ix;
     s->ix++;
-    *bufp = blob + 8;
+    // Jumbo already malloc'd, otherwise pass down to generic allocator.
+    if (jumbo) {
+	free(z->hdrBuf);
+	z->hdrBuf = blob;
+	z->hdrBufSize = 8 + dataSize;
+    }
+    else {
+	char *p = generic_opHdrBuf(z, 8 + dataSize);
+	if (!p)
+	    return ERRNO("malloc"), -1;
+	memcpy(p, blob, 8 + dataSize);
+    }
     return 8 + dataSize;
-}
-
-static ssize_t OP(NextMalloc)(struct zpkglistReader *z, int64_t *posp, const char *err[2])
-{
-    void *view;
-    ssize_t ret = OP(NextView)(z, &view, posp, err);
-    if (ret <= 0)
-	return ret;
-    size_t allocSize = 8 + ret + 8;
-#ifdef __SSE2__
-    allocSize += 24;
-#endif
-    char *p = generic_opHdrBuf(z, allocSize);
-    if (!p)
-	return ERRNO("malloc"), -1;
-    memcpy(p, view, allocSize);
-    return ret;
 }
 
 const struct ops OPS = {
